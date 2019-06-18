9665beeeed5904966a925faf320be849fce1682e
Vinh Doan-The <vinhdt@funix.edu.vn>
2019-06-18 18:30:46 +0700

    Setting without some default framework

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+config.frameworks -= [ :active_record, :active_resource, :action_mailer]
+
 # Initialize the Rails application.
 Rails.application.initialize!
